a3d81e0e79e08582f9b2f959bc1cd2bb92f8ffff
Mete Kabak <kabakmete@gmail.com>
2016-03-18 04:21:24 +0200

    fixing api versioning url problem

--- app/scripts/services/resource.js
+++ app/scripts/services/resource.js
@@ -10,7 +10,7 @@
  angular.module('homerunApp')
  .factory('Resource', function ($resource, $cacheFactory,$rootScope,$location) {
 
-   $rootScope.server = 'http://localhost:3000';
+   $rootScope.server = 'http://localhost:3000/v1';
    var cache = $cacheFactory('resourceCache');
 
    var interceptor = {
